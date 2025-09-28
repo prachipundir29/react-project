8124f98eadc3770b6774d07936d4eb26348f8e50
prachipundir29 <prachipundir64@gmail.com>
2025-09-28 21:36:39 +0530

    Parent Child Component

--- basic-react-practice/react-blog2/src/Components/ParentComponent.jsx
+++ basic-react-practice/react-blog2/src/Components/ParentComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ChildComponent from './ChildComponent'
 
 const ParentComponent = () => {
-    
+
     const displayName = (name) => {
         alert(name)
     }
@@ -14,11 +14,10 @@ const ParentComponent = () => {
     <>
      <h1>24. Call Parent Component Function From Child Component</h1>
      <div style={{display: "flex", gap: "10px", flexWrap: "wrap", marginBottom: "40px", marginTop:"20px"}}>
-      <ChildComponent displayName={displayName} name="Neha" />
-      <ChildComponent displayName={displayName} name="Raaj"  />
-      <ChildComponent displayName={displayName} name="Smith"  />
-      <ChildComponent displayName={displayName} name="Joe"  />
-      <ChildComponent getUser = {getUser}/>
+      <ChildComponent displayName={displayName} name="Neha"  getUser = {getUser} />
+      <ChildComponent displayName={displayName} name="Raaj"  getUser = {getUser}  />
+      <ChildComponent displayName={displayName} name="Smith"  getUser = {getUser}  />
+      <ChildComponent displayName={displayName} name="Joe"  getUser = {getUser}  />
       </div>
     </>
   )
